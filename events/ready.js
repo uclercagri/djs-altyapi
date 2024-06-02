c291070f714ace04e8e4e1370aeda0bdb47f0948
emre <96652518+emreconf@users.noreply.github.com>
2024-06-02 13:30:30 +0300

    Update ready.js
    Signed-off-by: emre <96652518+emreconf@users.noreply.github.com>

--- events/ready.js
+++ events/ready.js
@@ -1,34 +1,49 @@
 const { REST, Routes, ActivityType } = require('discord.js');
-const mongo = require('../database/mongo'); 
+const mongo = require('../database/mongo');
+const chalk = require('chalk');
+const figlet = require('figlet');
 
 module.exports = {
   name: 'ready',
   once: true,
   async execute(client) {
-    console.log(`Bot is online as ${client.user.tag}!`);
+    console.clear();
+
+    console.log(chalk.green(figlet.textSync('Harmoni', { horizontalLayout: 'full' })));
+
+    console.log(chalk.blue(`Bot is online as ${chalk.bold(client.user.tag)}!`));
 
     const commands = client.commands.map(command => command.data.toJSON());
 
     const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
     try {
-      console.log('Started refreshing application (/) commands.');
+      console.log(chalk.yellow('Started refreshing application (/) commands.'));
       await rest.put(
         Routes.applicationCommands(client.user.id),
         { body: commands },
       );
-      console.log('Successfully reloaded application (/) commands.');
+      console.log(chalk.green('Successfully reloaded application (/) commands.'));
     } catch (error) {
-      console.error(error);
+      console.error(chalk.red('Failed to reload application (/) commands:'), error);
     }
 
     mongo().then(() => {
-      console.log('Connected to MongoDB');
-    }).catch(console.error);
+      console.log(chalk.green('Connected to MongoDB'));
+    }).catch(error => {
+      console.error(chalk.red('Failed to connect to MongoDB:'), error);
+    });
+
+    const activities = [
+      { name: 'i am being developed', type: ActivityType.Playing },
+      { name: 'from dreamland', type: ActivityType.Playing },
+      { name: 'discord.gg/harmoni', type: ActivityType.Listening },
+    ];
+    const statusOptions = ['online', 'idle', 'dnd'];
 
     client.user.setPresence({
-      activities: [{ name: 'I am being developed', type: ActivityType.Playing }],
-      status: 'idle',
+      activities: [activities[Math.floor(Math.random() * activities.length)]],
+      status: statusOptions[Math.floor(Math.random() * statusOptions.length)],
     });
   },
 };
